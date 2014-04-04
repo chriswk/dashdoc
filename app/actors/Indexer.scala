ee0ba18edc7e3d891d4ea1c2d5a1c81155080577
chrkolst <christopher.kolstad@finn.no>
2014-04-04 13:11:21 +0200

    Able to index artifact coords and classnames

--- app/actors/Indexer.scala
+++ app/actors/Indexer.scala
@@ -1,5 +1,6 @@
 package actors
 
+import play.api.libs.concurrent.Execution.Implicits._
 import akka.actor.{Actor, ActorLogging}
 import com.sksamuel.elastic4s.ElasticClient
 import com.sksamuel.elastic4s.ElasticDsl._
@@ -12,6 +13,7 @@ case class GAV(groupId: String, artifactId: String, version: String, classifier:
     val b = s"${groupId}:${artifactId}:${version}"
     val p = classifier match {
       case Some(cl) => s"@${cl}"
+      case None => ""
     }
     b + p
   }
@@ -40,6 +42,8 @@ case class IndexClass(gav: GAV, className: String)
 
 case class IndexArtifact(gav: GAV)
 
+case class IndexComplete(index: String, id: String, indexType: String, version: Long)
+
 class Indexer extends Actor with ActorLogging {
   val client = ElasticClient.local
 
@@ -51,12 +55,15 @@ class Indexer extends Actor with ActorLogging {
           "name" -> repo.name
           "url" -> repo.url
         }
+      } onSuccess {
+        case res => self ! IndexComplete(res.getIndex, res.getId, res.getType, res.getVersion)
       }
     }
 
     case IndexArtifact(gav) => {
+      log.info(s"Indexing ${gav}")
       client.execute {
-        index into "artifacts" -> gav.toString fields {
+        index into "artifacts" -> gav.classifier.getOrElse("binary") fields {
           "artifactId" -> gav.artifactId
           "groupId" -> gav.groupId
           "version" -> gav.version
@@ -64,6 +71,8 @@ class Indexer extends Actor with ActorLogging {
           "path" -> gav.filePath
           "url" -> gav.url
         }
+      } onSuccess {
+        case res => self ! IndexComplete(res.getIndex, res.getId, res.getType, res.getVersion)
       }
     }
     case IndexClass(gav, className) => {
@@ -71,7 +80,13 @@ class Indexer extends Actor with ActorLogging {
         index into "classes" -> gav.toString fields {
           "class" -> className
         }
+      } onSuccess {
+        case res => sender ! IndexComplete(res.getIndex, res.getId, res.getType, res.getVersion)
       }
+    }
+
+    case comp@IndexComplete => {
+      log.error(s"Completed indexing of ${comp}")
     }
 
   }
--- build.sbt
+++ build.sbt
@@ -10,3 +10,4 @@ libraryDependencies ++= Seq(
 )
 
 play.Project.playScalaSettings
+
